d1b793e692309ad2c324c3067302e5e48a999121
tatutoru <dhierryerag@gmail.com>
2025-12-04 00:37:18 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,5 +1,6 @@
 fetch("menu.html")
-  .then(response => response.text())
-  .then(data => {
-     document.getElementById("menu-container").innerHTML = data;
-  });
+    .then(response => response.text())
+    .then(data => {
+        document.getElementById("menu-container").innerHTML = data;
+    })
+    .catch(err => console.error("Erro ao carregar o menu:", err));
