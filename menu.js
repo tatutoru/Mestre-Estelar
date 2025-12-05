dfc724943ee13706235cdd488b98d15140362b63
tatutoru <dhierryerag@gmail.com>
2025-12-05 07:48:05 -0300

    Update menu.js

--- menu.js
+++ menu.js
@@ -1,7 +1,6 @@
 fetch("/Mestre-Estelar/menu.html")
-  .then(response => {
-    if (!response.ok) throw new Error('HTTP ' + response.status);
-    return response.text();
+  .then(r => r.text())
+  .then(html => {
+    document.getElementById("menu-container").innerHTML = html;
   })
-  .then(data => document.getElementById("menu-container").innerHTML = data)
   .catch(err => console.error("Erro ao carregar o menu:", err));
